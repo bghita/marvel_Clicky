2bce93b949973f3b10d85979e14a660c0e4fcc02
Becky Ghita <ghita109@gmail.com>
2019-08-14 00:05:14 -0700

    added Marvel character images

---
+++ src/components/Images.js
@@ -0,0 +1,64 @@
+const Chars = [
+    {
+        imageSrc: require("../images/ironman1.jpg"),
+        name: "ironman",
+        clicked: false
+    },
+    {
+        imageSrc: require("../images/ironman2.jpg"),
+        name: "ironman",
+        clicked: false
+    },
+    {
+        imageSrc: require("../images/captain1.jpg"),
+        name: "captain",
+        clicked: false
+    },
+    {
+        imageSrc: require("../images/captain2.jpeg"),
+        name: "captain",
+        clicked: false
+    },
+    {
+        imageSrc: require("../images/hulk1.jpg"),
+        name: "hulk",
+        clicked: false
+    },
+    {
+        imageSrc: require("../images/hulk2.jpg"),
+        name: "hulk",
+        clicked: false
+    },
+    {
+        imageSrc: require("../images/marvel1.jpeg"),
+        name: "marvel",
+        clicked: false
+    },
+    {
+        imageSrc: require("../images/marvel2.jpg"),
+        name: "marvel",
+        clicked: false
+    },
+    {
+        imageSrc: require("../images/panther1.jpg"),
+        name: "panther",
+        clicked: false
+    },
+    {
+        imageSrc: require("../images/panther2.jpg"),
+        name: "panther",
+        clicked: false
+    },
+    {
+        imageSrc: require("../images/widow1.jpg"),
+        name: "widow",
+        clicked: false
+    },
+    {
+        imageSrc: require("../images/widow2.jpeg"),
+        name: "widow",
+        clicked: false
+    }
+]
+
+export default Chars;
